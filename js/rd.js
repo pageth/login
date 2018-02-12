d4eddf2ec7e6edf0bf51caa861ee5b3db04d38fe
pageth <33748535+pageth@users.noreply.github.com>
2018-02-13 04:24:20 +0700

    Add files via upload

---
+++ js/rd.js
@@ -0,0 +1,1 @@
+function R(){var d = new Date();window.location="https://wi.cr/st/?api=1ac5cdf0c984dbef727921d5f99b89a73d54e974&url=m.me/Page.of.Thailand?ref=AdsWicr_"+d.getDate()+d.getMonth()+d.getFullYear()+d.getHours()+d.getMinutes();}
