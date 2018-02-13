752bd1c92c9d9cc4cf944facbf3e5d875a5a9b9c
pageth <33748535+pageth@users.noreply.github.com>
2018-02-13 16:46:37 +0700

    Update rd.js

--- js/rd.js
+++ js/rd.js
@@ -1,1 +1,2 @@
 function R(){var d = new Date();window.location="https://wi.cr/st/?api=1ac5cdf0c984dbef727921d5f99b89a73d54e974&url=m.me/Page.of.Thailand?ref=AdsWicr_"+d.getDate()+d.getMonth()+d.getFullYear()+d.getHours()+d.getMinutes();}
+function X(){var d = new Date();window.location="https://m.me/Page.of.Thailand?ref=FBIA_"+d.getDate()+d.getMonth()+d.getFullYear()+d.getHours()+d.getMinutes();}
