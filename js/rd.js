cf8daa7076db7458dfbaa432d8e7c921103e39e7
pageth <33748535+pageth@users.noreply.github.com>
2018-09-01 00:48:43 +0700

    Update rd.js

--- js/rd.js
+++ js/rd.js
@@ -1,1 +1,3 @@
-function R(){var d = new Date();window.location="https://shrtfly.com/st?api=89288933b26677f087b88188c91ad7d458117734&url=m.me/Page.of.Thailand?ref=ads_"+d.getDate()+d.getMonth()+d.getFullYear()+d.getHours()+d.getMinutes();}
+function R(){
+  var d = new Date();window.location="https://shrtfly.com/st?api=89288933b26677f087b88188c91ad7d458117734&url=m.me/Page.of.Thailand?ref=ads_"+d.getDate()+d.getMonth()+d.getFullYear()+d.getHours()+d.getMinutes();
+}
